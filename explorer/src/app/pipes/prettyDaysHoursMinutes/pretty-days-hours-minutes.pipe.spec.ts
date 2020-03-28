aba6c57b76805bd23b50bc7e69c67bb213af2e96
orib <oribrau@gmail.com>
2020-03-28 18:45:12 +0300

    remove clients

--- explorer/src/app/pipes/prettyDaysHoursMinutes/pretty-days-hours-minutes.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PrettyDaysHoursMinutesPipe } from './pretty-days-hours-minutes.pipe';
-
-describe('PreetyDaysHoursMinutesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PrettyDaysHoursMinutesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- explorer/src/app/pipes/prettyTime/pretty-time.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PrettyTimePipe } from './pretty-time.pipe';
-
-describe('PrettyTimePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PrettyTimePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
