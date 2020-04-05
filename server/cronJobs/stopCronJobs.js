60d735b8ac5adce0239bfd757ff5db4224b94594
orib <oribrau@gmail.com>
2020-04-05 14:33:37 +0300

    added script to clear cron jobs

---
+++ server/cronJobs/stopCronJobs.js
@@ -0,0 +1,29 @@
+const settings = require('./../wallets/all_settings');
+const db = require('./../database/db');
+const fs = require('fs-extra');
+
+var string = "# /etc/cron.d/myCronTabs -- schedules wallet updates\n";
+for (var wallet in settings) {
+    // var txid = settings[wallet].example_txid;
+    // var hash = settings[wallet].example_hash;
+    // var dev_address = settings[wallet].dev_address;
+    //   * * * * *  sleep  0 && /usr/bin/node /var/www/html/server/cronJobs/update_split.js fix >> /var/www/html/logs/fixCronUpdate.log 2>&1
+    // */5 * * * *  sleep  0 && /usr/bin/node /var/www/html/server/cronJobs/update_peers.js fix >> /var/www/html/logs/fixCronUpdate.log 2>&1
+    // */5 * * * *  sleep  0 && /usr/bin/node /var/www/html/server/cronJobs/update_masternodes.js fix >> /var/www/html/logs/fixCronUpdate.log 2>&1
+    // string += "  * * * * * root sleep 0 && /usr/bin/node /var/www/html/chain.review/server/cronJobs/update_split.js " + wallet + " >> /var/www/html/chain.review/logs/"+ wallet + "CronUpdate.log 2>&1\n";
+    // string += "*/5 * * * * root sleep 0 && /usr/bin/node /var/www/html/chain.review/server/cronJobs/update_peers.js " + wallet + " >> /var/www/html/chain.review/logs/"+ wallet + "CronUpdate.log 2>&1\n";
+    // string += "*/5 * * * * root sleep 0 && /usr/bin/node /var/www/html/chain.review/server/cronJobs/update_masternodes.js " + wallet + " >> /var/www/html/chain.review/logs/"+ wallet + "CronUpdate.log 2>&1\n";
+    // string += "*/5 * * * * root sleep 0 && /usr/bin/node /var/www/html/chain.review/server/cronJobs/update_richlist.js " + wallet + " >> /var/www/html/chain.review/logs/"+ wallet + "CronUpdate.log 2>&1\n";
+    // string += "*/5 * * * * root sleep 0 && /usr/bin/node /var/www/html/chain.review/server/cronJobs/update_market.js " + wallet + " >> /var/www/html/chain.review/logs/"+ wallet + "CronUpdate.log 2>&1\n";
+    // string += "  * * * * * root sleep 0 && /usr/bin/node /var/www/html/chain.review/server/cronJobs/update_tx_chart.js " + wallet + " >> /var/www/html/chain.review/logs/"+ wallet + "CronUpdate.log 2>&1\n";
+    string += "\n\n";
+
+}
+
+fs.writeFile("/etc/cron.d/myCronTabs", string, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+});
+console.log(string);
