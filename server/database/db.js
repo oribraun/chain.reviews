a97efd7878c92317cd368b7467c25822b67a101e
orib <oribrau@gmail.com>
2020-07-27 15:25:04 +0300

    increasing timeout

--- server/database/db.js
+++ server/database/db.js
@@ -66,7 +66,8 @@ var db = {
         dbString = dbString + '/' + dbSettings.database;
         // console.log(dbString)
         var options = {
-            socketTimeoutMS: 10*60*1000, // 10 minute timeout
+            socketTimeoutMS: 20*60*1000, // 10 minute timeout
+            connectTimeoutMS: 20*60*1000, // 10 minute timeout
             keepAlive: true,
             useNewUrlParser: true,
             useUnifiedTopology: true,
