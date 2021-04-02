ca518716eae9cb10236fe0f0a8d2f3cd6238eff9
orib <oribrau@gmail.com>
2021-04-02 16:11:30 +0300

    remove address count

--- server/database/models/cluster.js
+++ server/database/models/cluster.js
@@ -4,15 +4,11 @@ var db = require('./../db');
 
 var ClusterSchema = new Schema({
   addresses: { type: Array, default: [] },
-  addresses_size: { type: Number, default: [], index: 1 },
   update: { type: Boolean, default: false },
   changed: { type: Boolean, default: false },
   tags: { type: Array, default: [] },
 }, {id: false, timestamps: false});
 
-ClusterSchema.post('update', (doc) => {
-  console.log('mongo update', doc);
-})
 var connections = db.getConnections();
 var obj = {};
 for(var i in connections) {
