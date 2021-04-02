38dfff8deadb5a85da943e8c851922d2a63baeb3
orib <oribrau@gmail.com>
2021-04-02 15:55:03 +0300

    added clusters watch

--- server/database/models/cluster.js
+++ server/database/models/cluster.js
@@ -4,6 +4,7 @@ var db = require('./../db');
 
 var ClusterSchema = new Schema({
   addresses: { type: Array, default: [] },
+  addresses_size: { type: Number, default: [], index: 1 },
   update: { type: Boolean, default: false },
   changed: { type: Boolean, default: false },
   tags: { type: Array, default: [] },
@@ -13,5 +14,9 @@ var connections = db.getConnections();
 var obj = {};
 for(var i in connections) {
   obj[i] = connections[i].model('Cluster', ClusterSchema);
+  const changeStream = obj[i].watch();
+  changeStream.on('change', data => {
+    console.log(new Date(), data)
+  });
 }
 module.exports = obj;
