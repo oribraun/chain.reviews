6f23c54fa3a5af8c9fcbb11d7bb311d718dc7b93
orib <oribrau@gmail.com>
2021-05-01 20:54:50 +0300

    allow multi process update

--- server/cronJobs/update_split.js
+++ server/cronJobs/update_split.js
@@ -71,7 +71,7 @@ function startReindex() {
     });
 }
 function startUpdateVinVout() {
-    var proc = spawn('/usr/bin/node', ['--max-old-space-size=8192', '/var/www/html/chain.review/server/sync.js', wallet, 'update_tx_vin_vout_and_addresses_cursor'], {execFileOpts, options}, function done(err, stdout, stderr) {
+    var proc = spawn('/usr/bin/node', ['--max-old-space-size=8192', '/var/www/html/chain.review/server/sync.js', wallet, 'update_tx_vin_vout_and_addresses'], {execFileOpts, options}, function done(err, stdout, stderr) {
         if (err) {
             console.error('Error:', err.stack);
             // reject(err.stack);
